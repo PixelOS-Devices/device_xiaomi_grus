d16b33d52995d1b8f84b57b4db525c33dead8b61
Tkiliay <sackclouths@gmail.com>
2024-07-26 23:16:21 +0800

    grus: Patch com.qti.chi.override.so The value 0x7D0 at 000863B0 only works in the miui camera buffer and causes interface lag in gcam, remove that buffer.
    before: MOV.W R2, #0x7D0 ; unsigned int
    
    after: MOV.W R2, #0 ; unsigned int
    
    Change-Id: I1f1da8d1f68d39126f7c026128831bc54e703615

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/hw/com.qti.chi.override.so)
+            "${SIGSCAN}" -p "4F F4 FA 62 11" -P "4F F0 00 02 11" -f "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
